0d681b6ef10747d07017d8e95ef0809eaf0b831a
Skoch33 <kochesem@gmail.com>
2023-09-09 12:40:09 +0300

    Добавлена локализация (Ru & Eng) для количества повторений

--- Tracker/ Cells/TrackerCell.swift
+++ Tracker/ Cells/TrackerCell.swift
@@ -65,7 +65,8 @@ final class TrackerCell: UICollectionViewCell {
     private var tracker: Tracker?
     private var days = 0 {
         willSet {
-            daysCountLabel.text = "\(newValue.days())"
+            daysCountLabel.text = String.localizedStringWithFormat(
+                NSLocalizedString("amountOfDays", comment: ""), newValue)
         }
     }
     
